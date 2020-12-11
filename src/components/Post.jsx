75e2586b624fce1fec3a5f980fd016ccf9e2e1e9
santiago echavarria rodriguez <sechavarria26@misena.edu.co>
2020-12-11 11:04:14 -0500

    error router solved

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,34 +9,44 @@ function Post() {
     <div className="container">
       <div className="col-12 justify-content-center">
         <div className="row justify-content-center">
-          <h1>
-            <strong>G2 Rekkles Confirmed</strong>
-          </h1>
+          <div className="col-12">
+            <h1>
+              <strong>G2 Rekkles Confirmed</strong>
+            </h1>
+          </div>
         </div>
         <br />
         <div className="row justify-content-center">
-          <img
-            src={g2rekkles}
-            className="img-fluid"
-            alt="Responsive in detail"
-          />
+          <div className="col-12">
+            <img
+              src={g2rekkles}
+              className="img-fluid"
+              alt="Responsive in detail"
+            />
+          </div>
         </div>
         <br />
         <div className="row justify-content-center">
-          <p>
-            Fnatic has lost their long-time AD Carry, Martin “Rekkles” Larsson.
-            According to Esportsmaníacos journalist Pablo “BloopGG” Suarez,
-            Rekkles is reportedly heading to G2 Esports for the 2021 season,
-            replacing Luka “Perkz” Perković. The veteran bot laner has spent the
-            eight years with Fnatic, achieving incredible results in the LEC and
-            at Worlds. While some may see this as a huge loss for Fnatic, it’s
-            only the beginning of some long overdue changes.
-          </p>
+          <div className="col-12">
+            <p>
+              Fnatic has lost their long-time AD Carry, Martin “Rekkles”
+              Larsson. According to Esportsmaníacos journalist Pablo “BloopGG”
+              Suarez, Rekkles is reportedly heading to G2 Esports for the 2021
+              season, replacing Luka “Perkz” Perković. The veteran bot laner has
+              spent the eight years with Fnatic, achieving incredible results in
+              the LEC and at Worlds. While some may see this as a huge loss for
+              Fnatic, it’s only the beginning of some long overdue changes.
+            </p>
+          </div>
         </div>
         <div className="row">
-          <Link to="/">
-            <button className="btn btn-outline-info">Back to the posts</button>
-          </Link>
+          <div className="col-12">
+            <Link to="/">
+              <button className="btn btn-outline-info">
+                Back to the posts
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
